17b5af0351955270198a5ef31a2c75f37de5db32
Your Name <you@example.com>
2024-06-13 14:50:49 +0700

    prak10

---
+++ Challenge10_22.11.4648.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>Praktikum 10</title>
+</head>
+<body>
+ <form method="POST" action="action.php">
+ Masukan nama anda : <input type="text" name="nama"> <br>
+ Masukkan angka pertama (1-10) : <input type="integer"> <br>
+ Masukkan angka kedua (1-10) : <input type="integer"> <br>
+ <input type="submit" value="Hitung">
+ </form>
+</body>
+</html>
